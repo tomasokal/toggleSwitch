d84df9f6dc3cbecd5f9f58546952aa80d8ad8bba
Tomas Okal <tomas.okal@gmail.com>
2022-12-27 12:16:28 -0600

    Add strictmode.

--- src/index.js
+++ src/index.js
@@ -5,15 +5,16 @@ import { createRoot } from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
 
 import Experience from './Experience.js'
+import { StrictMode } from 'react'
 
 const root = createRoot(document.querySelector('#root'))
 
 root.render(
-
+    <StrictMode>
         <Canvas>
             <Suspense fallback={null}>
                 <Experience /> 
             </Suspense>  
         </Canvas>
-
+    </StrictMode>
 )
